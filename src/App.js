e931c5c34b179ddf74ef81acd739ce6cbf9d0c5d
BinZ <sikatunar@gmail.com>
2022-10-03 09:38:02 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./index.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Cryptobridge from "./components/Cryptobridge";
+
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Cryptobridge />
     </>
   );
 }
